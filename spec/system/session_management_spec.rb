e1a13c155275f54bb9ed6e5328cc69c263bfb1f4
michaellennox <michaelctlennox@gmail.com>
2019-03-24 12:40:59 +0000

    Add simple system test for login into an account

--- spec/system/session_management_spec.rb
+++ spec/system/session_management_spec.rb
@@ -23,4 +23,32 @@ RSpec.describe "Session Management", type: :system do
     expect(page).not_to have_link "Sign up"
     expect(page).to have_link "Log out"
   end
+
+  it "allows a user to login to an existing account" do
+    pending
+    
+    user = FactoryBot.create(:user)
+
+    visit root_path
+
+    click_on "Log in"
+
+    within "#session-form" do
+      fill_in "Email", with: user.email
+      fill_in "Password", with: "notthepass"
+
+      click_on "Log in"
+    end
+
+    expect(page).to have_content "Invalid email or password"
+
+    within "#session-form" do
+      fill_in "Password", with: user.password
+
+      click_on "Log in"
+    end
+
+    expect(page).not_to have_link "Log in"
+    expect(page).to have_link "Log out"
+  end
 end
