c4477b1beb6a343a84184698d0ab667bcfdbf953
michaellennox <michaelctlennox@gmail.com>
2019-03-24 11:38:16 +0000

    Only display sign up / login in nav bar when not authenticated

--- spec/system/session_management_spec.rb
+++ spec/system/session_management_spec.rb
@@ -3,17 +3,24 @@
 require "rails_helper"
 
 RSpec.describe "Session Management", type: :system do
-  it "allows a user to create an account from a link on the hompage" do
-    pending
-
+  it "allows a user to register an account" do
     visit root_path
 
     click_on "Sign up"
 
-    fill_in "Display Name", with: "Joe Blogs"
     fill_in "Email", with: "joe.blogs@example.com"
     fill_in "Password", with: "SuperPassword123!"
 
-    expect(page).not_to have_link("Sign up")
+    click_on "Create User"
+
+    expect(page).to have_content "Display name can't be blank"
+
+    fill_in "Display Name", with: "Joe Blogs"
+    fill_in "Password", with: "SuperPassword123!"
+
+    click_on "Create User"
+
+    expect(page).not_to have_link "Sign up"
+    expect(page).to have_link "Log out"
   end
 end
